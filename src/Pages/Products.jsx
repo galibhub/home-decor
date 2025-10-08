1cc5040f8a493fd5a15af9b1c0f181a3cf38bedf
IBRAHIM AHMED GALIB <ibrahimgalib00@gmail.com>
2025-10-08 12:30:27 +0600

    Implemented All Product showing functionality using custom hooks

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,9 +1,24 @@
 import React from 'react';
+import useProducts from '../hooks/useProducts';
+import { Link } from 'react-router';
+import ProductCard from '../Components/ProductCard';
 
 const Products = () => {
+    const {products}=useProducts()
     return (
         <div>
-            <h1>Products</h1>
+           
+            <div className='flex justify-between items-center py-5'>
+                <h1 className='text-3xl font-semibold'>All Products: {products.length}</h1>
+                <Link className='btn btn-outline' to='/products'>Search</Link>
+            </div>
+           <div className='grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4'>
+             {
+               products.map(product=>
+               <ProductCard key={product.id} product={product}></ProductCard>
+                )
+            }
+           </div>
         </div>
     );
 };
